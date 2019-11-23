49e697ad68d9d499a6e227db2d2885743b63405b
anthony <anthony@localhost>
2019-11-23 18:59:25 +0000

    Fix crash on following post link

--- ThreadWindow.m
+++ ThreadWindow.m
@@ -49,6 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	NSLog(@"Fetched detailed thread");
 	[networkSource release];
 	networkSource = nil;
+	highlightedPost = nil;
 	[displayedThread release];
 	displayedThread = detailedThread;
 	[displayedThread retain];
