7c98bc6f55115c134eef9a60b7b50ed76ff699f5
anthony <anthony@localhost>
2019-12-23 20:18:07 +0000

    Display media from tmp cache if possible

--- MediaManager.m
+++ MediaManager.m
@@ -35,11 +35,18 @@ static NSString *const TMP_DIR = @"surfboard";
 		NSLog(@"Already downloading media, skipping");
 		return;
 	}
-	[currentObserver mediaBeganLoading];
-	networkSource = [[DataNetworkSource alloc] initWithURL: aURL];
-	[networkSource performOnSuccess: @selector(onFetchMedia:) target: self];
-	[networkSource performOnFailure: @selector(onFetchFail:) target: self];
-	[networkSource fetch];
+	BOOL openedFromCache = [self tryToDisplayCachedFileForURL: aURL];
+	if (openedFromCache) {
+		NSLog(@"Viewed file from cache, won't make request");
+	} else {
+		[currentObserver mediaBeganLoading];
+		networkSource = [[DataNetworkSource alloc] initWithURL: aURL];
+		[networkSource performOnSuccess: @selector(onFetchMedia:) target:
+			self];
+		[networkSource performOnFailure: @selector(onFetchFail:) target:
+			self];
+		[networkSource fetch];
+	}
 }
 
 -(void)setObserver: (NSObject<MediaLoadingObserver>*)anObserver {
@@ -52,17 +59,12 @@ static NSString *const TMP_DIR = @"surfboard";
 	[currentObserver mediaFinishedLoading];
 	[self setObserver: nil];
 	NSLog(@"on fetched media");
+	NSString *tmp = [self filePathForStoredMediaURL: [networkSource URL]];
+	[data writeToFile: tmp atomically: NO];
 	if ([AppUserDefaults isInternalViewerEnabled]) {
 		NSLog(@"Internal viewer is enabled in prefs");
 		[self tryInternalViewerToViewMedia: data];
 	} else {
-		NSString *fileName = [[networkSource URL] lastPathComponent];
-		NSString *tmp = [NSTemporaryDirectory()
-			stringByAppendingPathComponent: TMP_DIR];
-		[[NSFileManager defaultManager] createDirectoryAtPath: tmp
-			attributes: nil];
-		tmp = [tmp stringByAppendingPathComponent: fileName];
-		[data writeToFile: tmp atomically: NO];
 		BOOL opened = [[NSWorkspace sharedWorkspace] openFile: tmp];
 		if (!opened) {
 			NSLog(@"Failed to temporarily save media to %@", tmp);
@@ -81,10 +83,48 @@ static NSString *const TMP_DIR = @"surfboard";
 	networkSource = nil;
 }
 
--(void)tryInternalViewerToViewMedia: (NSData*)data {
+-(BOOL)tryToDisplayCachedFileForURL: (NSURL*)aURL {
+	NSString *path = [self filePathForStoredMediaURL: aURL];
+	BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath: path];
+	NSLog(@"path for cached media: %@, fileExists: %d", path, fileExists);
+	// Return early, no saved file.
+	if (!fileExists) {
+		return NO;
+	}
+	BOOL preferInternalViewer = [AppUserDefaults isInternalViewerEnabled];
+	// Try nsworkspace first and return on success
+	if (!preferInternalViewer) {
+		if ([[NSWorkspace sharedWorkspace] openFile: path]) {
+			return YES;
+		}
+	}
+	// Didn't succeed, so fall back to internal viewer
+	NSData *data = [[NSFileManager defaultManager] contentsAtPath: path];
+	return [self tryInternalViewerToViewMedia: data];
+}
+
+-(BOOL)tryInternalViewerToViewMedia: (NSData*)data {
 	NSLog(@"Falling back to internal viewer");
+	if (data == nil) {
+		NSLog(@"Data was nil");
+		return NO;
+	}
 	NSImage *image = [[[NSImage alloc] initWithData: data] autorelease];
-	[imageWindow loadImage: image];
+	if (image != nil) {
+		[imageWindow loadImage: image];
+		return YES;
+	} else {
+		return NO;
+	}
+}
+
+-(NSString*)filePathForStoredMediaURL: (NSURL*)aURL {
+	NSString *fileName = [aURL lastPathComponent];
+	NSString *tmp = [NSTemporaryDirectory()
+		stringByAppendingPathComponent: TMP_DIR];
+	[[NSFileManager defaultManager] createDirectoryAtPath: tmp attributes: 
+		nil];
+	return [tmp stringByAppendingPathComponent: fileName];
 }
 
 @end
